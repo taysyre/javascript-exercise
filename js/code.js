6d603a2116b320a0a3a5b038fb2c739395fce037
taysyre <tayapril3@gmail.com>
2022-11-08 10:31:34 +0200

    Javascript exercise

---
+++ js/code.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Please enter your name");
+let lastName = prompt("Please enter your surname");
+let userSalary = prompt("Please enter your salary");
+let salarySum = eval(userSalary) + 500;
+document.write(`Name:${firstName} <br>`);
+document.write(`Surname:${lastName} <br>`);
+document.write(`Salary:${salarySum} <br>`);
